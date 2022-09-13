795d4caf5b549842268f6db47ad6941ba9cc4d9f
paullaster <paullasterokoth98@gmail.com>
2022-09-13 16:21:10 +0300

    exploring destructuring

---
+++ obj_prop.js
@@ -0,0 +1,16 @@
+const a=30;
+const b=50;
+const c=70;
+const d =90;
+
+const e = {
+    a,
+    b,
+    c,
+    d
+}
+
+const {a: f1, b: f2, c: f3, d: f4} = e;
+
+const fValues = {f1, f2, f3, f4};
+console.log(fValues);
