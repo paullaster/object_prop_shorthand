17ce3bcc797f43fcf797c749718e03e13f6a9ec9
paullaster <paullasterokoth98@gmail.com>
2022-09-13 16:23:38 +0300

    adding func object vreator

--- obj_prop.js
+++ obj_prop.js
@@ -14,3 +14,13 @@ const {a: f1, b: f2, c: f3, d: f4} = e;
 
 const fValues = {f1, f2, f3, f4};
 console.log(fValues);
+
+function objectCreator(x,y,z){
+    return {
+        x,
+        y,
+        z
+    }
+}
+
+console.log(objectCreator(20,40,60));
